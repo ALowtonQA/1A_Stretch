ac4094523c8ff5fa3b79e2a267b785b6137faf66
ALowtonQA <alowton@qa.com>
2024-02-27 16:51:35 +0000

    AL: lab 3

---
+++ Solutions/Lab3/part1/countvowels.py
@@ -0,0 +1,16 @@
+userWord = input("Enter a word\n")
+
+vowels = 0
+index = 0
+
+# while (index < len(userWord)):
+#     if (userWord[index] == "a" or userWord[index] == "e" or userWord[index] == "i" or userWord[index] == "o" or userWord[index] == "u"):
+#         vowels += 1
+#     index += 1
+
+
+for letter in userWord:
+    if letter in "aeiou":
+        vowels += 1
+
+print("The number of vowels in", userWord, "is:", vowels)
