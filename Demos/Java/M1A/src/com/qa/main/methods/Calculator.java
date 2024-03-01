f9a92a9fb7162da6e3ceb6acc21f5e6e6d79234b
ALowtonQA <alowton@qa.com>
2024-03-01 15:07:50 +0000

    AL: methods

---
+++ Demos/Java/M1A/src/com/qa/main/methods/Calculator.java
@@ -0,0 +1,9 @@
+package com.qa.main.methods;
+
+public class Calculator {
+
+	public int add(int a, int b) {
+		return a + b;
+	}
+	
+}
---
+++ Demos/Java/M1A/src/com/qa/main/methods/Printer.java
@@ -0,0 +1,9 @@
+package com.qa.main.methods;
+
+public class Printer {
+
+	public void sayHello(String name) {
+		System.out.println("Hello " + name);
+	}
+	
+}
